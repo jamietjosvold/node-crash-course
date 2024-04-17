0b8ac87d448390f1988b56b8b9291faabb1b0b6f
Tjosie <jamietjosvold.school@gmail.com>
2024-04-17 15:14:22 -0600

    EJS

--- app.js
+++ app.js
@@ -4,27 +4,37 @@ const express = require('express');
 
 const app = express();
 
+// register view engine
+
+app.set('view engine', 'ejs');
+
 // listen for requests
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>Welcome!</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home' });
 })
+
 app.get('/about', (req, res) => {
     // res.send('<p>Welcome!</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
+})
+
+app.get('/create', (req, res) => {
+    // res.send('<p>Welcome!</p>');
+    res.render('create', { title: 'Blog' });
 })
 
 // redirects
 
 app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+    res.render('about');
 })
 
 // 404 page - app.use grabs any request that makes it past everything above
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 })
